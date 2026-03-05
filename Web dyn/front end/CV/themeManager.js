9bb952c0ed7f3d0b8179ecf2d447c92f8ad2c074
Lounès Amichi <122723239+Lounes-Amichi@users.noreply.github.com>
2026-03-05 20:30:13 +0100

    CV
    - Added styles.css
    - Added index.css
    - Added themeManager.js

---
+++ Web dyn/front end/CV/themeManager.js
@@ -0,0 +1,18 @@
+function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+}
+
+function toggleTheme() {
+    if (localStorage.getItem("theme") == "light") {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
+}
+
+window.onload = (event) => {
+    if (localStorage.getItem("theme") != null)
+        setTheme(localStorage.getItem("theme"));
+    else toggleTheme();
+}
